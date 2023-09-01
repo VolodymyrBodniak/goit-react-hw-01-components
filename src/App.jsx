243d112af16b6e25d23f87295824e05435e6d113
VolodymyrBodniak <bodnjak@gmail.com>
2023-09-01 21:48:05 +0400

    Update task4

--- src/App.jsx
+++ src/App.jsx
@@ -1,10 +1,13 @@
 import { Profile } from 'components/Profile/Profile';
 import { Statistics } from 'components/Statistics/Statistics';
 import { FriendList } from 'components/FriendList/FriendList';
+import { TransactionHistory } from 'components/Transactoins/Transactions';
+import { TransactionData } from 'components/Transactoins/TransactionData/TransactionData';
 
 import user from 'data/user.json';
 import data from 'data/data.json';
 import friends from 'data/friends.json';
+import transactions from 'data/transactions.json';
 
 export const App = () => {
   return (
@@ -17,7 +20,10 @@ export const App = () => {
         avatar={user.avatar}
       />
       <Statistics title="Upload stats" stats={data} />
-      <FriendList friends={friends} />;
+      <FriendList friends={friends} />
+      <TransactionHistory>
+        <TransactionData items={transactions} />
+      </TransactionHistory>
     </>
   );
 };
